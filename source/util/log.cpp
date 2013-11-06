f0d17c0461445f7ecafe845e3ac2c780083e69c5
JerryZhou <JerryZhou@outlook.com>
2013-11-06 18:45:44 +0800

    fix localtime in windows

--- source/util/log.cpp
+++ source/util/log.cpp
@@ -60,7 +60,7 @@ int __sc_log_write(int prio, const char *tag, const char *text){
     struct tm *tm;
     
     gettimeofday(&tv, NULL);
-    tm=localtime(&tv.tv_sec);
+    tm=localtime((time_t*)(&tv.tv_sec));
     if (tag) {
      return printf("%s%d:%02d:%02d %d \t %s \t %s%s\n", __sc_colors[prio],
                    tm->tm_hour, tm->tm_min, tm->tm_sec, tv.tv_usec, tag, text, __sc_colors[0]);
