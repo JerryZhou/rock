26ce97c2dc0b075f8b70f583c284b89fb57bd8f5
JerryZhou <JerryZhou@outlook.com>
2013-11-06 18:50:02 +0800

    move qt dependency

--- source/main.cpp
+++ source/main.cpp
@@ -1,14 +1,16 @@
-#include <QMainWindow>
-#include <QApplication>
+//#include <QMainWindow>
+//#include <QApplication>
 
 #include "test/testmain.h"
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication a(argc, argv);
+    (void)argc;
+    (void)argv;
+    //QCoreApplication a(argc, argv);
     
     // unit test entry
     test_main();
     
-    return a.exec();
+    return 0;//a.exec();
 }
--- source/stable.h
+++ source/stable.h
@@ -1,5 +1,5 @@
 #ifndef __STABLE_H_
 #define __STABLE_H_
-#include <QtGui>
-#include <QtNetwork>
+//#include <QtGui>
+//#include <QtNetwork>
 #endif
